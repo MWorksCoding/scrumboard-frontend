0f54a29ef61c112d386b5baf1238c8b41af2ef01
MWorksCoding <mail.org@posteo.org>
2024-04-21 21:15:11 +0200

    building login

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,23 +1,56 @@
 import { Component } from '@angular/core';
-import {MatCardModule} from '@angular/material/card';
-import {MatButtonModule} from '@angular/material/button';
-import {MatFormFieldModule} from '@angular/material/form-field';
-import {MatInputModule} from '@angular/material/input';
+import { MatCardModule } from '@angular/material/card';
+import { MatButtonModule } from '@angular/material/button';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { MatIconModule } from '@angular/material/icon';
+import {MatCheckboxModule} from '@angular/material/checkbox';
+import { FormsModule } from '@angular/forms';
+import {
+  animate,
+  state,
+  style,
+  transition,
+  trigger,
+} from '@angular/animations';
 
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [MatCardModule, MatButtonModule, MatFormFieldModule, MatInputModule],
+  imports: [
+    MatCardModule,
+    MatButtonModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatIconModule,
+    MatCheckboxModule,
+    FormsModule
+  ],
   templateUrl: './login.component.html',
-  styleUrl: './login.component.scss'
+  styleUrl: './login.component.scss',
+  animations: [
+    trigger('fadeInOut', [
+      state(
+        'void',
+        style({
+          opacity: 0,
+        })
+      ),
+      transition('void <=> *', animate('0.5s ease-in-out')),
+    ]),
+  ],
 })
 export class LoginComponent {
-
   loading: boolean = false;
   failedLogin: boolean = false;
   username: string = '';
   password: string = '';
-
+  loginCard: boolean = true;
+  signupCard: boolean = false;
+  forgotPasswordCard: boolean = false;
+  wrongEmail: boolean = true;
+  wrongEntries: boolean = true;
+  rememberMe: boolean = true;
 
   /**
    * Communication to backend
@@ -59,5 +92,29 @@ export class LoginComponent {
     if (loadingScreen) {
       loadingScreen.style.display = 'none';
     }
+  }
+
+  showSignupCard() {
+    this.loginCard = false;
+    this.forgotPasswordCard = false;
+    setTimeout(() => {
+      this.signupCard = true;
+    }, 500);
+  }
+
+  showLoginCard() {
+    this.signupCard = false;
+    this.forgotPasswordCard = false;
+    setTimeout(() => {
+      this.loginCard = true;
+    }, 500);
+  }
+
+  showForgotPasswordCard() {
+    this.signupCard = false;
+    this.loginCard = false;
+    setTimeout(() => {
+      this.forgotPasswordCard = true;
+    }, 500);
   }
 }
