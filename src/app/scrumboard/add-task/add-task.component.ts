c18b9b34cf52ae30fe4138a3b609c10290c0ed7f
MWorksCoding <mail.org@posteo.org>
2024-05-15 20:41:44 +0200

    get requests for add-task-page

--- src/app/scrumboard/add-task/add-task.component.ts
+++ src/app/scrumboard/add-task/add-task.component.ts
@@ -3,24 +3,37 @@ import { MatButtonModule } from '@angular/material/button';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatInputModule } from '@angular/material/input';
 import { MatIconModule } from '@angular/material/icon';
-import {DateAdapter, MAT_DATE_LOCALE} from '@angular/material/core';
-import {MatSelectModule} from '@angular/material/select';
-import {MatDatepickerModule} from '@angular/material/datepicker';
-import {provideNativeDateAdapter} from '@angular/material/core';
-import { FormControl, FormGroupDirective, NgForm, Validators, FormsModule, ReactiveFormsModule, } from '@angular/forms';
-import {provideMomentDateAdapter} from '@angular/material-moment-adapter'
-import { MatMomentDateModule } from "@angular/material-moment-adapter";
+import { DateAdapter, MAT_DATE_LOCALE } from '@angular/material/core';
+import { MatSelectModule } from '@angular/material/select';
+import { MatDatepickerModule } from '@angular/material/datepicker';
+import { provideNativeDateAdapter } from '@angular/material/core';
+import {
+  FormControl,
+  FormGroupDirective,
+  NgForm,
+  Validators,
+  FormsModule,
+  ReactiveFormsModule,
+} from '@angular/forms';
+import { provideMomentDateAdapter } from '@angular/material-moment-adapter';
+import { MatMomentDateModule } from '@angular/material-moment-adapter';
 import * as _moment from 'moment';
-import {default as _rollupMoment} from 'moment';
+import { default as _rollupMoment } from 'moment';
 import { MatCardModule } from '@angular/material/card';
+import { environment } from '../../../environments/environments';
+import { lastValueFrom } from 'rxjs';
+import { HttpClient } from '@angular/common/http';
 
 const moment = _rollupMoment || _moment;
 
 @Component({
   selector: 'app-add-task',
   standalone: true,
-  providers: [provideMomentDateAdapter()],
-  imports: [MatButtonModule,
+  // providers: [provideMomentDateAdapter()],
+  // providers: [{provide: MAT_DATE_LOCALE, useValue: 'en-GB'}],
+  providers: [provideNativeDateAdapter()],
+  imports: [
+    MatButtonModule,
     MatFormFieldModule,
     MatCardModule,
     MatInputModule,
@@ -29,30 +42,86 @@ const moment = _rollupMoment || _moment;
     FormsModule,
     MatSelectModule,
     MatDatepickerModule,
-    ReactiveFormsModule],
+    ReactiveFormsModule,
+  ],
   templateUrl: './add-task.component.html',
-  styleUrl: './add-task.component.scss'
+  styleUrl: './add-task.component.scss',
 })
 export class AddTaskComponent {
+  // date = new FormControl(moment(new Date()).format('DD.MM.YYYY'));
+  date = new FormControl(new Date());
+  categories: { id: number, name: string, color: string }[] = [];
+  users: { id: number, first_name: string, last_name: string, username: string, color: string }[] = [];
 
-  date = new FormControl(moment(new Date()).format('DD.MM.YYYY'));
-  foods = [
-    {value: 'sales', viewValue: 'Sales'},
-    {value: 'design', viewValue: 'Design'},
-    {value: 'backoffice', viewValue: 'Backoffice'},
-    {value: 'marketing', viewValue: 'Marketing'},
-    {value: 'it', viewValue: 'IT'},
-    {value: 'media', viewValue: 'Media'},
-  ];
+  title = '';
+  description = '';
+  category = '';
+  priority = '';
+  user = '';
 
-  users = [
-    {value: 'user-0', viewValue: 'User1'},
-    {value: 'user-1', viewValue: 'User2'},
-    {value: 'user-2', viewValue: 'User3'},
-  ];
+  constructor(private http: HttpClient) {}
 
-  constructor(
-  ) {  }
+  /**
+   * Initialization
+   */
+  ngOnInit() {
+    this.fetchCategories();
+    this.fetchUsers();
+  }
 
+  async fetchCategories() {
+    try {
+      const url = environment.baseUrl + '/categories/';
+      const response = await lastValueFrom(this.http.get(url));
+      this.categories = response as any[];
+    } catch (error) {
+      console.error('Error fetching categories:', error);
+    }
+  }
+  
+  async fetchUsers() {
+    try {
+      const url = environment.baseUrl + '/users/';
+      const response = await lastValueFrom(this.http.get(url));
+      this.users = response as any[];
+    } catch (error) {
+      console.error('Error fetching users:', error);
+    }
+  }
 
+  async addTask() {
+    console.log(
+      'this.title:',
+      this.title,
+      'this.description:',
+      this.description,
+      'this.category:',
+      this.category,
+      'priority:',
+      this.priority,
+      'assigned_to:',
+      this.user
+    );
+    try {
+      const url = environment.baseUrl + '/add-task/';
+      const body = {
+        title: this.title,
+        description: this.description,
+        due_date: this.date.value,
+        category: this.category,
+        priority: this.priority,
+        assigned_to: this.user,
+      };
+      console.log('body', body);
+      const response = await lastValueFrom(this.http.post(url, body));
+      this.clearInputFields();
+    } catch (error) {
+      console.error('Error creating todo:', error);
+    }
+  }
+
+  clearInputFields() {
+    this.title = '';
+    this.description = '';
+  }
 }
--- src/app/scrumboard/summary/summary.component.ts
+++ src/app/scrumboard/summary/summary.component.ts
@@ -23,7 +23,7 @@ export class SummaryComponent {
   }
 
   /**
-   * Initialization / activate dark mode
+   * Initialization
    */
   ngOnInit() {
     this.getDataFromBackend();
